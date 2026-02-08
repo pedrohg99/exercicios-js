3381cb9e1e1bab2473c9f9a7040eb7db734564c2
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 15:28:11 -0300

    Exercicio 1 - contagem de numeros

---
+++ revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
@@ -0,0 +1,9 @@
+// Você está criando um aplicativo para exibir a sequência de números até um valor que o usuário forneceu. O objetivo é mostrar todos os números a partir de 1 até o valor informado pelo usuário, de forma ordenada e crescente.
+// Como você pode gerar essa contagem e garantir que cada número seja mostrado até atingir o valor final fornecido?
+
+const numeroFinal = 5;
+
+for (let i = 1; i <= numeroFinal; i++) {
+    console.log(i);
+              
+} 
