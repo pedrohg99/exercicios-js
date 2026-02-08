de4ea6acbe73f5b60306ef306663cdc85fe83b61
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 10:38:31 -0300

    Ecercicio 9 - Verificar acesso a nivel do jogo

--- exercicio.js
+++ exercicio.js
@@ -189,3 +189,17 @@
             console.log('Opção inválida')
             break;
     }
+    
+    //Você está criando um sistema para um jogo, onde o personagem tem que passar por duas condições para conseguir avançar para o próximo nível:
+    // O personagem precisa ter mais de 50 pontos.
+    // O personagem deve ter pelo menos uma vida restante.
+    // Crie um programa que, dado os pontos e as vidas restantes, verifique se o personagem pode ou não avançar para o próximo nível.
+
+    const pontos = 50;
+    const vidas = 5;
+
+    if (pontos >= 50 && vidas >= 1) {
+        console.log('Próximo nível liberado!');
+    } else {
+        console.log('Não pode avançar para o próximo nível.');
+    }
