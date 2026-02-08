5af1142e15215a0c87d3978b6b6b7af46060bd9b
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 16:09:47 -0300

    Exercicio 7 - Somando os primeiros N numeros

--- revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
+++ revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
@@ -56,3 +56,22 @@ for (let i = 1; i <= 20; i++) {
   console.log(i);
 }
 
+//Você está desenvolvendo uma funcionalidade para um aplicativo de finanças pessoais. Um dos recursos permite simular quanto uma pessoa economizaria em um período de tempo, iniciando em R$1 no primeiro dia e aumentando esse valor em R$1 a cada novo dia. Ou seja, a cada dia que passa, ela economiza R$1 a mais do que no anterior.
+// Por exemplo, em 10 dias ela economizaria:
+// Dia 1: R$1
+// Dia 2: R$3
+// Dia 3: R$6
+// ...
+// Dia 10: R$55
+// Crie um programa que calcule o total economizado ao final de 10 dias.
+
+const totalDias = 10;
+let dia = 1;
+let economia = 0;
+ 
+do {
+  economia += dia;
+  dia++;
+} while (dia <= totalDias);
+ 
+console.log(`Total economizado: R$ ${economia}`);
