da9e685248893f86da2f8a1dbffdf11b1f922d03
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 10:49:10 -0300

    Exercicio 10 - Mostra categoria da bateria

--- exercicio.js
+++ exercicio.js
@@ -203,3 +203,14 @@
     } else {
         console.log('Não pode avançar para o próximo nível.');
     }
+
+    //Você está desenvolvendo um sistema que exibe a situação da bateria de um dispositivo com base na porcentagem de carga. O sistema deve classificar a bateria nas seguintes categorias:
+    // "Crítica": se a bateria estiver abaixo de 20%
+    // "Moderada": se estiver entre 20% e 80%
+    // "Cheia": se estiver acima de 80%
+    // Utilize o operador ternário para definir e exibir a categoria da bateria com base no valor da variável bateria.
+
+    const bateria = 20;
+
+    const categoria = (bateria < 20) ? 'Crítica' : (bateria <= 80) ? 'Moderada' : 'Cheia';
+    console.log(categoria);
