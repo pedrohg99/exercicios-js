7f58594f37ec6fe3a638bab0cd2d846fe5bee207
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 15:31:41 -0300

    Exercício 2 - Contagem regressiva

--- revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
+++ revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
@@ -6,4 +6,12 @@ const numeroFinal = 5;
 for (let i = 1; i <= numeroFinal; i++) {
     console.log(i);
               
-} 
+} ;
+
+// Você é uma pessoa desenvolvedora de interface em uma startup de tecnologia aeroespacial e precisa criar uma contagem regressiva automática para o painel de lançamento. A contagem deve começar de 10 até 0, exibindo “Lançar!” ao final.
+// Crie um programa que conte de 10 até 0 e exiba a mensagem “Lançar!” ao final.
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i);    
+}
+console.log('Lançar!');
