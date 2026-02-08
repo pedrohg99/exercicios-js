c001bb0317ef9e6965c48152a198944c1c449d62
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 10:22:36 -0300

    Exercicio 6 - compatibilidade de frutas com suco (ternario)

--- exercicio.js
+++ exercicio.js
@@ -153,3 +153,12 @@
     const usuario = "admin";
     const userPermission = (usuario === 'admin') ? 'Login bem-sucedido!' : 'Usuário inválido.';
     console.log(userPermission);
+
+    //Você está desenvolvendo um sistema para verificação de compatibilidade de frutas em um processo de preparo de sucos. O sistema precisa verificar se o tipo de fruta informado é compatível com o tipo esperado, de acordo com uma receita.
+    // O suco pode ser feito somente com "laranja" ou "abacaxi".
+    // Qualquer outra fruta que não seja "laranja" ou "abacaxi" deve gerar uma mensagem de erro, informando que a fruta não é compatível.
+    // Crie um programa que, dado o nome da fruta, verifique se ela é compatível com a receita.
+
+    const fruta = 'kiwi';
+    const suco = (fruta === 'laranja') || (fruta === 'abacaxi') ? 'Fruta compatível para a receita.' : 'Fruta incompatível para a receita.';
+    console.log(suco);
