7eb93db9a1ddf2cc44dd472266a956300cdd974a
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 16:20:39 -0300

    Exercicio 8 - Temporizador de aquecimento

--- revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
+++ revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
@@ -75,3 +75,25 @@ do {
 } while (dia <= totalDias);
  
 console.log(`Total economizado: R$ ${economia}`);
+
+//Você está programando o temporizador de uma esteira aquecida para secagem de produtos. O sistema precisa manter a esteira aquecida por pelo menos 5 segundos, mesmo que a temperatura ideal já tenha sido atingida.
+// O painel deve exibir, segundo a segundo:
+// “Aquecendo... segundo X” a cada ciclo;
+// A mensagem "Temperatura ideal atingida." exatamente no segundo em que essa condição for alcançada;
+// E ao final, o total de segundos que o sistema permaneceu ligado.
+// Crie um programa que simule esse funcionamento do temporizador de aquecimento, garantindo que ele continue funcionando até atingir pelo menos 5 segundos.
+
+const tempoMinimo = 5;
+const temperaturaIdealAlcancadaEm = 3;
+
+for (let segundos = 1; segundos <= tempoMinimo; segundos++) {
+  console.log(`Aquecendo... segundo ${segundos}`);
+
+  if (segundos === temperaturaIdealAlcancadaEm) {
+    console.log("Temperatura ideal atingida.");
+  }
+
+  if (segundos === tempoMinimo) {
+    console.log(`Tempo total de aquecimento: ${segundos} segundos`);
+  }
+}
