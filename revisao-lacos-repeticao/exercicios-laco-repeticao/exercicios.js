ff0e97e7013571e46424638f02ad17272994376b
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 16:01:12 -0300

    Exercicio 6 - Verificando acesso restrito

--- revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
+++ revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
@@ -44,3 +44,15 @@ while (entradas[i] !== "fim") {
     console.log('Nome: ', entradas[i]);
     i++;    
 }
+
+//Você está trabalhando no sistema de controle de acesso de um laboratório secreto. Por questões de segurança, o número 10 deve ser evitado a todo custo. O contador de testes deve exibir os números normalmente, mas precisa ser encerrado imediatamente ao chegar nesse número.
+// Crie um programa que inicie a contagem em 1 e siga até 20. Se o número 10 for alcançado, o sistema deve exibir uma mensagem de alerta e interromper a contagem.
+
+for (let i = 1; i <= 20; i++) {
+  if (i === 10) {
+    console.log("Número proibido encontrado! Encerrando...");
+    break;
+  }
+  console.log(i);
+}
+
