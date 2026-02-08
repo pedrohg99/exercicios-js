363b41aaefbfb6c1ada1dc7c0c12737825cd923f
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-08 15:49:05 -0300

    Exercicio 5 - Solicitando nomes ate digirar 'fim'

--- revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
+++ revisao-lacos-repeticao/exercicios-laco-repeticao/exercicios.js
@@ -33,3 +33,14 @@ const senha = 'Corinthi@ans';
 for (let i = 0; i < senha.length; i++) {
     console.log(`Caractere ${i + 1} : ${senha[i]}`);
 }
+
+//Você está criando um sistema de cadastro em que a pessoa usuária pode cadastrar quantos nomes quiser, um por vez. O processo deve continuar até que seja encontrado a palavra "fim" (com letras minúsculas).
+// Crie um programa que com base em uma entrada pré-definida de nomes, exiba cada um deles, e encerre somente quando encontrar o valor "fim".
+
+const entradas = ["Ana", "Bruno", "Carla", "fim", "Daniel"];
+let i = 0;
+
+while (entradas[i] !== "fim") {
+    console.log('Nome: ', entradas[i]);
+    i++;    
+}
