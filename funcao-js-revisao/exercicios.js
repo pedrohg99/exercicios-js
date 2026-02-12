7fb6e8a252f55aeb0b1f64388acbe891ac3b51b0
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-12 17:21:32 -0300

    Cria segundo exercicio da revisao

--- funcao-js-revisao/exercicios.js
+++ funcao-js-revisao/exercicios.js
@@ -14,3 +14,13 @@ console.log (calculaPotencia(4, 3));
 console.log (calculaPotencia(4, 3));
 console.log (calculaPotencia(5, 5));
 console.log (calculaPotencia(5, 5));
+
+//criar função que calcula o fatorial de um número usando recursão.
+// fatorial: n! nultiplicação de n por seu antecessores maiores ou iguais a 1 
+// ex: 5! = 5 x 4 x 3 x 2 x 1
+
+const fatorial = function f(num) {
+    if (num === 0 || num === 1) return 1;
+    return num * f(num -1);
+}
+console.log (fatorial(5));
