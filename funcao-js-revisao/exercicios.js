de46054ebc38da1f2786749bf40e24e51fb32355
Pedro Henrique <pedrohg99@yahoo.com>
2026-02-12 17:31:21 -0300

    Cria exercicio de funcao com arrow function

--- funcao-js-revisao/exercicios.js
+++ funcao-js-revisao/exercicios.js
@@ -24,3 +24,13 @@ const fatorial = function f(num) {
     return num * f(num -1);
 }
 console.log (fatorial(5));
+
+// criar uma função para calcular juros compostos 
+// função deve receber os valores no formato inteiro: valor, % de juros e tempo 
+// fórmula: valor * (juro elevado a tempo)
+
+const calculaJuros = (valor, juros, tempo) => {
+    let taxaJuros = (juros/100) + 1;
+    return valor * Math.pow(taxaJuros, tempo);
+}
+console.log(calculaJuros(1000, 5, 2));
