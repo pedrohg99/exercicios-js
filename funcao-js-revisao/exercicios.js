a293b8417e157e82c1652f0dc92f6eaee281a75b
Pedro Henrique <pedrohg99@yahoo.com>
2026-03-17 11:18:43 -0300

    Cria exercicio fatorial com funcao recursiva

--- funcao-js-revisao/exercicios.js
+++ funcao-js-revisao/exercicios.js
@@ -16,8 +16,11 @@ console.log(calculaPotencia(4, 5));
 //criar função que calcula o fatorial de um número usando recursão.
 // fatorial: n! nultiplicação de n por seu antecessores maiores ou iguais a 1 
 // ex: 5! = 5 x 4 x 3 x 2 x 1
-
-
+const fatorial = function f(num){
+    if (num === 0 || num === 1) return 1;
+    return num * f(num - 1);
+}
+console.log(fatorial(5));
 
 
 // criar uma função para calcular juros compostos 
