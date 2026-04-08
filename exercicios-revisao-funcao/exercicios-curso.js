e760505e5b8a4a3644f6a5ce58a5bfff46bd39fd
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-08 13:05:34 -0300

    cria exercicio de mensagem personalizada

--- exercicios-revisao-funcao/exercicios-curso.js
+++ exercicios-revisao-funcao/exercicios-curso.js
@@ -35,3 +35,22 @@ const calcularFrete (distancia) => {
     }
 }
 console.log(calcularFrete (35));
+
+
+
+function processarPedido(nome, tipoCliente, callback) {
+    console.log(`Processando pedido de ${nome}...`);
+    callback(nome, tipoCliente);
+}
+ 
+function mensagemPersonalizada(nome, tipo) {
+    if (tipo === "vip") {
+        console.log(`Obrigado pela preferência, ${nome}! Você ganhou frete grátis.`);
+    } else if (tipo === "novo") {
+        console.log(`Bem-vindo(a), ${nome}! Aproveite um cupom de boas-vindas.`);
+    } else {
+        console.log(`Obrigado pela sua compra, ${nome}!`);
+    }
+}
+ 
+processarPedido("Lucas", "vip", mensagemPersonalizada);
