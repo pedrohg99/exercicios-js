d2d76accef45f3a6408d6269de1d746a185f9f00
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-08 11:54:40 -0300

    cria segundo exercicio calculando desconto

--- exercicios-revisao-funcao/exercicios-curso.js
+++ exercicios-revisao-funcao/exercicios-curso.js
@@ -7,3 +7,11 @@ function saudacao(nome = "") {
 }
 
 saudacao("Lucas");
+
+function calcularDesconto(preco, desconto = 10){
+    const valorFinal = preco - (preco * desconto / 100);
+    return valorFinal;
+}
+
+console.log(calcularDesconto(100, 20));
+console.log(calcularDesconto(100));
