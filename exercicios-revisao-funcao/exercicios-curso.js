a70957c157fd86cb753a93dfc33446f9d785d057
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-13 10:10:06 -0300

    cria exercicio contagem regressiva recursiva.

--- exercicios-revisao-funcao/exercicios-curso.js
+++ exercicios-revisao-funcao/exercicios-curso.js
@@ -149,5 +149,16 @@ const participantes = [
 
 realizarSorteio(participantes);
 
+function contagemRegressiva (numero){
+    if (numero > 0){
+        console.log(numero);
+        contagemRegressiva (numero - 1);
+
+    } else {
+        console.log("Lançamento!");
+    }
+}
+contagemRegressiva(5);
+
 
     
