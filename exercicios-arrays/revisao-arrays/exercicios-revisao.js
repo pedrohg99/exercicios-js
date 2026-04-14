5e040ccc652452306a5782cdaa950eac2e754066
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-14 16:11:58 -0300

    Cria exercicio de filtragem de arrays e alteracao de valores especificos

--- exercicios-arrays/revisao-arrays/exercicios-revisao.js
+++ exercicios-arrays/revisao-arrays/exercicios-revisao.js
@@ -30,3 +30,12 @@ arr1[0][0] = 5;
 
 console.log(arr1);
 console.log(arr2);
+
+
+//filtrar um array e alterar valores específicos
+// ex: alterar números para strings
+
+const cpfs = ['12323434545', '34658756786', 43254365434, '76547865345', 56768767564];
+const result = cpfs.map(cpf => typeof cpf === 'string' ? cpf : cpf.toString())
+
+console.log(result);
