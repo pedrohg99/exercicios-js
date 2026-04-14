289eba2fbd16617ef4bfd156ff6ce78a8463b7d2
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-14 16:52:00 -0300

    Cria exercicio Percorrendo listas com for

--- exercicios-arrays/exercicios-arrays/exercicios-arrays.js
+++ exercicios-arrays/exercicios-arrays/exercicios-arrays.js
@@ -4,3 +4,9 @@ listaDeCompras[listaDeCompras.length - 1] = 'fruta';
 
 console.log ("Lista após a modificação:", listaDeCompras);
 
+const despesas = [120, 80, 45.5, 200, 60];
+let total = 0;
+for (let i = 0; i < despesas.length; i++) {
+    total += despesas[i];
+}
+console.log("Total de despesas: ", total);
