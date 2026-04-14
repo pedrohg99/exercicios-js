64496ceb2195286c5a8cf4ab17de92bcbe13dc98
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-14 16:01:32 -0300

    Cria exercicio de revisao deep search

--- exercicios-arrays/revisao-arrays/exercicios-revisao.js
+++ exercicios-arrays/revisao-arrays/exercicios-revisao.js
@@ -10,3 +10,23 @@ function criaArrNum(inicio, fim){
 }
 
 console.log(criaArrNum(12, 30));
+
+const arr1 = [[1, 2], 2, 3];
+
+const copiaArray = (arr) => {
+    const copia = [];
+    arr.forEach((elem) => {
+        if (Array.isArray(elem)) {
+            copia.push(copiaArray(elem));
+        } else {
+            copia.push(elem);
+        }
+    })
+    return copia;
+}
+
+const arr2 = copiaArray(arr1);
+arr1[0][0] = 5;
+
+console.log(arr1);
+console.log(arr2);
