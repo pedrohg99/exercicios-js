1d0c78e26ccbe0a28cce168d048555c4375e9075
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-14 16:43:39 -0300

    Cria exercicio acessando e modificando elementos de um array

---
+++ exercicios-arrays/exercicios-arrays/exercicios-arrays.js
@@ -0,0 +1,6 @@
+const listaDeCompras = ['arroz', 'feijão', 'macarrão', 'tomate'];
+console.log ("Segundo item da lista:", listaDeCompras[1]);
+listaDeCompras[listaDeCompras.length - 1] = 'fruta';
+
+console.log ("Lista após a modificação:", listaDeCompras);
+
