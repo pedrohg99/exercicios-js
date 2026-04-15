236ddc1e605cded7ddfa6e09d2da4c483f2da8ae
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-15 09:16:35 -0300

    Cria exercicio filtrando valores

--- exercicios-arrays/exercicios-arrays/exercicios-arrays.js
+++ exercicios-arrays/exercicios-arrays/exercicios-arrays.js
@@ -45,3 +45,19 @@ if (livros.indexOf(livroProcurado) !== -1){
 const precos = [100, 80, 50, 120];
 let precosDesconto = precos.map(precos => precos * 0.9);
 console.log("Preços com desconto: ", precosDesconto);
+
+const participantes = [
+  { nome: 'Ana', idade: 17 },
+  { nome: 'Bruno', idade: 22 },
+  { nome: 'Carla', idade: 19 },
+  { nome: 'Daniel', idade: 15 },
+  { nome: 'Eduarda', idade: 25 }
+];
+
+const autorizados = participantes.filter(p => {
+    p.idade >= 18 ? console.log(`Acesso libarado para: ${p.nome}`) : false; return p.idade >= 18
+});
+
+const aprovados = autorizados.map(p => p.nome);
+
+console.log("lista de aprovados", aprovados);
