90a1e8918c1e60d11d477f03fa46f6e7cf72ddb5
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-16 12:17:06 -0300

    Cria exercicio Analisando desempenho de estudantes

--- objetos-js/exercicios-objetos/exercicios-objetos.js
+++ objetos-js/exercicios-objetos/exercicios-objetos.js
@@ -78,27 +78,6 @@ function montarObjeto(dado){
 
 console.log(montarObjeto(dados));
 
-// MEU EXERCICIO
-const relatorio = {
-  temperatura: 75,
-  vibracao: 40,
-  pressao: 55,
-  nivelRuido: 30
-};
-
-console.log("Categorias avaliadas: ", Object.keys(relatorio));
-console.log("Valores Registrados: ", Object.values(relatorio));
-console.log("Detalhamento:");
-
-for (const [chave, valor] of Object.entries(relatorio)){
-    if (valor > 50){
-        console.log (`${chave}: Alerta!`);
-    } else {
-        console.log (`${chave}: Ok!`);
-    }
-}
-
-// GABARITO
 const relatorio = {
   temperatura: 75,
   vibracao: 40,
@@ -134,12 +113,10 @@ let mesesComParada = 0;
 for (let mes in manutencao) {
   const dias = manutencao[mes];
   totalDias += dias;
-
   if (dias > 0) {
     mesesComParada++;
   }
 }
-
 console.log(`Total de dias parados: ${totalDias}`);
 console.log(`Meses com manutenção registrada: ${mesesComParada}`);
 
@@ -148,3 +125,32 @@ if (totalDias > 20) {
 } else {
   console.log("Status: Dentro do limite anual");
 }
+
+
+
+const curso = {
+  titulo: "JavaScript Intermediário",
+  estudantes: [
+    { nome: "João", progresso: 85 },
+    { nome: "Ana", progresso: 40 },
+    { nome: "Juliano", progresso: 72 },
+    { nome: "Patrícia", progresso: 60 }
+  ],
+
+  gerarRelatorio: function () {
+    let totalProgresso = 0;
+    for (let estudante of this.estudantes) {
+      const situacao = estudante.progresso >= 70 ? "Aprovado" : "Em andamento";
+      console.log(`Estudante: ${estudante.nome} | Progresso: ${estudante.progresso}% | Situação: ${situacao}`);
+      totalProgresso += estudante.progresso;
+    }
+
+    const media = (totalProgresso / this.estudantes.length).toFixed(2);
+
+    console.log(`\nTotal de estudantes: ${this.estudantes.length}`);
+    console.log(`Média geral da turma: ${media}%`);
+  }
+};
+
+curso.gerarRelatorio();
+
