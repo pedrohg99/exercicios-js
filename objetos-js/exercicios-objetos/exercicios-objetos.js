3d461cd3ae675507de4192f7c1280e36818e3b1a
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-16 11:28:51 -0300

    Cria exericio Criando um sistema de inspecao

--- objetos-js/exercicios-objetos/exercicios-objetos.js
+++ objetos-js/exercicios-objetos/exercicios-objetos.js
@@ -77,3 +77,47 @@ function montarObjeto(dado){
 }
 
 console.log(montarObjeto(dados));
+
+// MEU EXERCICIO
+const relatorio = {
+  temperatura: 75,
+  vibracao: 40,
+  pressao: 55,
+  nivelRuido: 30
+};
+
+console.log("Categorias avaliadas: ", Object.keys(relatorio));
+console.log("Valores Registrados: ", Object.values(relatorio));
+console.log("Detalhamento:");
+
+for (const [chave, valor] of Object.entries(relatorio)){
+    if (valor > 50){
+        console.log (`${chave}: Alerta!`);
+    } else {
+        console.log (`${chave}: Ok!`);
+    }
+}
+
+// GABARITO
+const relatorio = {
+  temperatura: 75,
+  vibracao: 40,
+  pressao: 55,
+  nivelRuido: 30
+};
+
+const categorias = Object.keys(relatorio);
+const valores = Object.values(relatorio);
+const entradas = Object.entries(relatorio);
+
+console.log("Categorias avaliadas:", categorias);
+console.log("Valores registrados:", valores);
+
+console.log("\nDetalhamento:");
+for (let [chave, valor] of entradas) {
+  const status = valor > 50 ? "alerta" : "ok";
+  console.log(`${chave}: ${valor} (${status})`);
+}
+
+
+
