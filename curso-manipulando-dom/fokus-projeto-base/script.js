50dbd5c66bde8cc58622157dc77a446796453c5f
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-17 10:46:29 -0300

    Cria funcao para reestruturar os eventos

--- curso-manipulando-dom/fokus-projeto-base/script.js
+++ curso-manipulando-dom/fokus-projeto-base/script.js
@@ -2,16 +2,21 @@ const html = document.querySelector('html') // relaciona a tag HTML do .html par
 const focoBt = document.querySelector('.app__card-button--foco') // relaciona as classes dos botões para inserir o evento dos clicks
 const curtoBt = document.querySelector('.app__card-button--curto') // relaciona as classes dos botões para inserir o evento dos clicks
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 
-focoBt.addEventListener('click', () => { //evento de click
-    html.setAttribute('data-contexto', 'foco') // pega o atributo HTML, seta o atributo data-contexto, e altera a propriedade para as configurações do 'foco' que está no CSS.
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco')
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    alterarContexto('descanso-curto')
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    alterarContexto('descanso-longo')
 })
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `imagens/${contexto}.png`)
+}
