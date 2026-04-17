42083e79059f1cfb92f3868267e36e8842c87243
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-17 13:40:35 -0300

    Finaliza botao inserindo o comando para alterar de comecar para pause. muda o icone para pause e play quando acionado, configura os tempos de cada evento e formata o tempo com new Date para o formato ideal.

--- curso-manipulando-dom/fokus-projeto-base/script.js
+++ curso-manipulando-dom/fokus-projeto-base/script.js
@@ -7,12 +7,15 @@ const titulo = document.querySelector('.app__title')// relaciona a classe do tí
 const botoes = document.querySelectorAll('.app__card-button') // pega todos os elementos que estão com a classe app__card-button
 const sartPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica') // pega o ID do botão que ativa a musica
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarBtIcone = document.querySelector('.app__card-primary-butto-icon')
+const tempoNaTela = document.querySelector('#timer')
 const musica = new Audio('sons/luna-rise-part-one.mp3') // adiciona ao JS o arquivo de audio com new Audio
 const musicaPlay = new Audio('sons/play.wav')
 const musicaPausa = new Audio('sons/pause.mp3')
 const audioTempoFinalizado = new Audio('sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 5 //let inicia o temporizador em 5 segundos
+let tempoDecorridoEmSegundos = 1500//let inicia o temporizador em 5 segundos
 let intervaloId = null // let inicia o temporizador
 
 musica.loop = true;
@@ -28,21 +31,25 @@ musicaFocoInput.addEventListener('change', () => {//pega a const musicaFocoInput
 })
 
 focoBt.addEventListener('click', () => { //cria evento click (efeito ao clicar no botao) inserindo a função alterarContexto para fazer o efeito de alterar cor de fundo e imagem.
+    tempoDecorridoEmSegundos = 15000 // configura o tempo de 25min (15000s) para o contexto foco
     alterarContexto('foco')
     focoBt.classList.add('active')//classList.add adiciona classe. Ou seja, ao clicar no botão, ele adiciona a classe 'active' do CSS, para que o botão herde a classe e fique visualmente selecionado.
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300 // configura o tempo de 5min (300s) para o contexto descanso-curto
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900 // configura o tempo de 15min (900s) para o contexto descanso-longo
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    mostrarTempo()
     botoes.forEach(function (contexto){// seleciona a variavel botoes (criada para puxar a classe dos botões). Utiliza o forEach para percorrer todos os botões e alterar cada um criando a função contexto.
         contexto.classList.remove('active')// remove o estilo active de acordo com o contexto (clique).
     })
@@ -70,7 +77,7 @@ const contagemRegressiva = () => { // configura o temporizador para que seja dec
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+    mostrarTempo() // mostra o tempo na tela constantemente
 }
 
 sartPauseBt.addEventListener('click', iniciarOuPausar) // cria evento de click para inicar o temporizador.
@@ -83,11 +90,22 @@ function iniciarOuPausar() {// inicia o temporizador criando o intervalo de 1s p
     }
     musicaPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+    iniciarOuPausarBtIcone.setAttribute ('src', 'imagens/pause.png')
 }
 
 function zerar(){//função criada para finalizar a execução do código
     clearInterval(intervaloId)//metodo clearInterval para de executar o intervaloId
+    iniciarOuPausarBt.textContent = "Começar"
+    iniciarOuPausarBtIcone.setAttribute ('src', 'imagens/play_arrow.png')
     intervaloId = null //retorna null para parar a execução da contagem
 }
 
+function mostrarTempo() { // função converte o tempo em pt-br, ou seja, ao invés de 1500s vai aparecer 25min
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000) // multiplica tempo decorrido em segundos para minutos
+    const tempoFormatado = tempo.toLocaleString('pt-Br', {minute: '2-digit', second: '2-digit'}) //formata o tempo que aparecerá na tela
+    tempoNaTela.innerHTML = `${tempoFormatado}` // imprime o tempo na tela
+}
+
+mostrarTempo() //mostra o tempo na tela já convertido
 
