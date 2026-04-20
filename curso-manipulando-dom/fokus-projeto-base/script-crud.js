105fa3b95b641ec98ba88fba4ed5d6ce05e8577e
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-20 10:46:19 -0300

    Recebe referencia de layout em tarefa.html e monta uma funcao para criar a lista de tarefas no JS

--- curso-manipulando-dom/fokus-projeto-base/script-crud.js
+++ curso-manipulando-dom/fokus-projeto-base/script-crud.js
@@ -1,7 +1,33 @@
+const { createElement } = require("react")
+
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const tarefas = []
+
+function criarElementoTarefa (tarefa) {
+    const li = document.createElement('li') // cria o <li> baseado na referencia do html apresentado em tarefa.html.
+    li.classList.add('app__section-task-list-item') // adiciona uma classe css ao <li>
+    const svg = document.createElement('svg') // cria o SVG que esta na referencia html, em tarefa.html
+    svg.innerHTML// cria elemento SVG utilizando o innerHTML = 
+    `
+        <svg>
+            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+                <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+            </svg>
+        </svg>
+    `
+    const paragrafo = document.createElement('p') // cria elemento P (parágrafo)
+    paragrafo.textContent = tarefa.descricao // paragrafo receberá o que for digitado no formulário, objeto guardado em tarefa.
+    const botao = document.createElement('button') // cria botão
+    const imagemBotao = document.createElement('img') // puxa a imagem do botão
+    imagemBotao.setAttribute('src', 'imagens/edit.png') // aplica a imagem do botão no atributo src
+    botao.append(imagemBotao) //append encaixa elemento imagem no botão
+    li.append(svg) //append encaixa elemento criado em SVG na lista
+    li.append(paragrafo)//append encaixa elemento criado em parágrafo na lista
+    li.append(botao)//append encaixa elemento criado em botão na lista 
+}
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
@@ -15,4 +41,6 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa) // joga os valores do array para dentro da const tarefa
     localStorage.setItem('tarefas', JSON.stringify(tarefas)) // guarda a lista inteira de tarefas. JSON.stringfy transforma o objeto em string, guardando o valor adicionado.
 })
+
+
 
