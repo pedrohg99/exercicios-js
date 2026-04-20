a0ea4854124576275dffd8f9287d2d52a0727630
Pedro Henrique <pedrohg99@yahoo.com>
2026-04-20 08:41:49 -0300

    Cria comando para o formulario nao se comportar como padrao ao enviar uma tarefa com preventDefault(). cria o array de tarefas para guardar a lista de tarefas cadastradas no form. cria variavel para guardar a tarefa cadastrada. utiliza o push para adicionar os valores dentro do array. usa o localStorage para guardar as tarefas cadastradas e usa o JSON.stringfy para corrigir a forma de armazenamento dos valores da lista, transformando em string.

--- curso-manipulando-dom/fokus-projeto-base/script-crud.js
+++ curso-manipulando-dom/fokus-projeto-base/script-crud.js
@@ -1,7 +1,17 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
-
+const textarea = document.querySelector('.app__form-textarea')
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const tarefa = { //guarda a tarefa cadastrada
+        descricao: textarea.value // cria um objeto com o valor da tarefa
+    }
+    tarefas.push(tarefa) // joga os valores do array para dentro da const tarefa
+    localStorage.setItem('tarefas', JSON.stringify(tarefas)) // guarda a lista inteira de tarefas. JSON.stringfy transforma o objeto em string, guardando o valor adicionado.
 })
